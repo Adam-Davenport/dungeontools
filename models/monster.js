fc5a30acf590af65dd29480ce45334141f43a461
adam <adavenpo@purdue.edu>
2017-01-06 19:39:22 -0500

    Creating the monster model

--- models/monster.js
+++ models/monster.js
@@ -1,3 +1,24 @@
-/**
- * Created by adam on 1/6/17.
- */
+// Monster Model
+var mongoose = require("mongoose");
+var monsterSchema = new mongoose.Schema({
+	name: String,
+	health: String,
+	type: String,
+	size: String,
+	ac: Number,
+	speed: String,
+	str: Number,
+	dex: Number,
+	con: Number,
+	int: Number,
+	wis: Number,
+	cha: Number,
+	prof: Number,
+	saving: String,
+	skills: String,
+	senses: String,
+	languages: String,
+	challenge: String,
+	actions: Array
+});
+module.exports = mongoose.model("Campground", campgroundSchema);
