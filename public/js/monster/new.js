b6209864d3ecc909cbc99418387a0f6bf9a547cc
adam <adavenpo@purdue.edu>
2017-01-11 16:37:32 -0500

    Added functionality to create an actionlist

--- public/js/monster/new.js
+++ public/js/monster/new.js
@@ -1,5 +1,7 @@
 var scores = document.getElementsByClassName("ability"),
-		prof = document.getElementById("prof");
+		prof = document.getElementById("prof"),
+		actionList,
+		actions
 
 updateScores();
 setProf();
@@ -8,6 +10,7 @@ setProf();
 function updateScores(){
 	for(i=0; i<scores.length; i++){
 		createOption(scores[i], 0, 40)
+		scores[i].selectedIndex = 10;
 	}
 }
 
@@ -23,4 +26,23 @@ function createOption(select, i, j){
 		i++;
 	}
 	return select;
+}
+
+function addAction(){
+	getActions()
+	var action = document.createElement('input')
+	action.type = 'text'
+	action.classList.add('action')
+	action.classList.add('form-control')
+	insertAfter(action, actions[actions.length-1])
+	action.name = 'monster[actions][' + String(actions.length-1) + ']'
+}
+
+function getActions(){
+	actionList = document.getElementsByClassName('actionList')
+	actions = document.getElementsByClassName('action')
+}
+
+function insertAfter(newNode, referenceNode) {
+	referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
 }
