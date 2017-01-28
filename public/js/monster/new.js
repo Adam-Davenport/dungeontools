a33ad9a8a81aa0f8cb68726dd22e4333466dbef4
adam <adavenpo@purdue.edu>
2017-01-28 12:49:05 -0500

    Populating type select

--- public/js/monster/new.js
+++ public/js/monster/new.js
@@ -1,31 +1,33 @@
-var scores = document.getElementsByClassName("ability"),
-		prof = document.getElementById("prof"),
+var scores = document.getElementsByClassName('ability'),
+		prof = document.getElementById('prof'),
+		types = document.getElementById('type'),
 		actionList,
 		actions
 
-updateScores();
-setProf();
+updateScores()
+setProf()
+setTypes()
 
 
 function updateScores(){
 	for(i=0; i<scores.length; i++){
 		createOption(scores[i], 0, 40)
-		scores[i].selectedIndex = 10;
+		scores[i].selectedIndex = 10
 	}
 }
 
 function setProf() {
-	createOption(prof, 1, 6);
+	createOption(prof, 1, 6)
 }
 
 function createOption(select, i, j){
 	while(i<j){
-		var option = document.createElement("option");
-		option.text = i;
-		select.add(option);
-		i++;
+		var option = document.createElement('option')
+		option.text = i
+		select.add(option)
+		i++
 	}
-	return select;
+	return select
 }
 
 function addAction(){
@@ -44,5 +46,34 @@ function getActions(){
 }
 
 function insertAfter(newNode, referenceNode) {
-	referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+	referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling)
+}
+
+function setTypes() {
+	var typesList = getTypes()
+	for(i=0; i<typesList.length; i++){
+		var type = document.createElement('option')
+		type.text = typesList[i]
+		type.value = typesList[i]
+		types.add(type)
+	}
+}
+
+function getTypes() {
+	return typesList = [
+		'Aberation',
+		'Beast',
+		'Celestial',
+		'Construct',
+		'Dragon',
+		'Elemental',
+		'Fey',
+		'Fiend',
+		'Giant',
+		'Humanoid',
+		'Monstrosity',
+		'Ooze',
+		'Plant',
+		'Undead'
+	]
 }
