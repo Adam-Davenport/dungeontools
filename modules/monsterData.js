5ca31966261c8439c2820b3f57273cd80587b6d7
Adam Davenport <adavenpo@purdue.edu>
2017-01-31 14:03:51 -0500

    Created data file to populate the information for monsters

---
+++ modules/monsterData.js
@@ -0,0 +1,6 @@
+var monster = {
+	sizes: ['Tiny', 'Small', 'Medium', 'Large', 'Huge', 'Gargantuan', 'Colossal'],
+	types: ['Aberation','Beast','Celestial','Construct','Dragon','Elemental','Fey','Fiend','Giant','Humanoid','Monstrosity','Ooze','Plant','Undead']
+}
+
+module.exports = monster
