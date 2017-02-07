778c4d2414509a1dbc6d044c051e5f6086258510
Adam Davenport <adavenpo@purdue.edu>
2017-02-07 16:11:50 -0500

    Removing unnecessary functions

--- public/js/monster/main.js
+++ public/js/monster/main.js
@@ -1,27 +1,6 @@
 var scores = document.getElementsByClassName('ability'),
-	types = document.getElementById('type'),
 	actionList,
 	actions
-
-function setScores(){
-	for(i=0; i<scores.length; i++){
-		createOption(scores[i], 0, 40)
-		scores[i].selectedIndex = 10
-	}
-}
-
-
-
-function createOption(select, i, j){
-	while(i<j){
-		var option = document.createElement('option')
-		option.text = i
-		option.value = i
-		select.add(option)
-		i++
-	}
-	return select
-}
 
 function addAction(){
 	getActions()
@@ -42,49 +21,3 @@ function insertAfter(newNode, referenceNode) {
 	referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling)
 }
 
-function setTypes() {
-	var typesList = getTypes()
-	for(i=0; i<typesList.length; i++){
-		var type = document.createElement('option')
-		type.text = typesList[i]
-		type.value = typesList[i]
-		types.add(type)
-	}
-}
-
-function getTypes() {
-	return typesList = [
-		'Aberation',
-		'Beast',
-		'Celestial',
-		'Construct',
-		'Dragon',
-		'Elemental',
-		'Fey',
-		'Fiend',
-		'Giant',
-		'Humanoid',
-		'Monstrosity',
-		'Ooze',
-		'Plant',
-		'Undead'
-	]
-}
-
-function setLevels() {
-	var level = document.getElementById('level')
-	createOption(level,1, 26)
-}
-
-function setHD() {
-	var hd = document.getElementById('hd')
-	for(i=4; i<16; i=i+2){
-		if(i==14){
-			i = 20
-		}
-		var option = document.createElement('option')
-		option.text = 'd' + i
-		option.value = i
-		hd.add(option)
-	}
-}
